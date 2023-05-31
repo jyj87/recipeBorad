153e6e09e829f9ab5e2c9294d23862b86baef28c
Jun87 <g.jyj87@gmail.com>
2023-06-01 07:49:17 +0900

    recipe 게시판 만들기 ajax + jpa + 파일 첨부 + 페이징 + 댓글

---
+++ src/main/java/com/example/recipeBoard/entity/BoardEntity.java
@@ -0,0 +1,58 @@
+package com.example.recipeBoard.entity;
+
+import com.example.recipeBoard.dto.BoardDTO;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+// DB의 테이블 역활을 하는 클래스
+@Entity
+@Getter
+@Setter
+@Table(name = "board_table")
+public class BoardEntity extends BaseEntity {
+    @Id // pk
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // auto_increment
+    private Long id;
+
+    @Column(length = 20, nullable = false) // 크기 20, notnull
+    private String boardWriter;
+
+    @Column // 디폴트 크기 255,null 가능
+    private String boardPass;
+
+    @Column
+    private String boardTitle;
+
+    @Column(length = 500)
+    private String boardContents;
+
+    @Column
+    private int boardHits;
+
+    // insert(save) 경우 Id 값이 필요하지 않음(pk db에서 자동 생성)
+    public static BoardEntity toSaveEntity(BoardDTO boardDTO){
+
+        BoardEntity boardEntity = new BoardEntity();
+        boardEntity.setBoardWriter(boardDTO.getBoardWriter());
+        boardEntity.setBoardPass(boardDTO.getBoardPass());
+        boardEntity.setBoardTitle(boardDTO.getBoardTitle());
+        boardEntity.setBoardContents(boardDTO.getBoardContents());
+        boardEntity.setBoardHits(0);
+        return boardEntity;
+    }
+
+    // update 경우 update의 대상을 찾기위해 Id 값이 필요
+    public static BoardEntity toUpdateEntity(BoardDTO boardDTO){
+
+        BoardEntity boardEntity = new BoardEntity();
+        boardEntity.setId(boardDTO.getId());
+        boardEntity.setBoardWriter(boardDTO.getBoardWriter());
+        boardEntity.setBoardPass(boardDTO.getBoardPass());
+        boardEntity.setBoardTitle(boardDTO.getBoardTitle());
+        boardEntity.setBoardContents(boardDTO.getBoardContents());
+        boardEntity.setBoardHits(boardDTO.getBoardHits());
+        return boardEntity;
+    }
+}
